f4398f4024cc9d110a2bb0f23b1a4f2427317c69
ahmedfarghaly261 <ahmedfarghaly201055@gmail.com>
2026-02-27 22:52:46 +0200

    feat: Set up shadcn/ui with core components, utilities, and theme configuration.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ function App() {
 
   return (
     <>
-     <h1 className='text-2xl text-red-500'>hello</h1>
+     <h1 className='text-2xl text-red-500 bg-background'>hello</h1>
+     <div className="bg-card">
+      <h1>test</h1>
+     </div>
     </>
   )
 }
