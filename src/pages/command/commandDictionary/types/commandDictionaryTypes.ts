c037a5d8004023fa6e6f46afd240ba96f3743766
ahmedfarghaly261 <ahmedfarghaly201055@gmail.com>
2026-05-08 19:24:48 +0300

    refactor command dictionary service and types

--- src/pages/command/commandDictionary/types/commandDictionaryTypes.ts
+++ src/pages/command/commandDictionary/types/commandDictionaryTypes.ts
@@ -1,3 +1,8 @@
+export interface CommandDictionarySubsystem {
+  hex_code: string;
+  name: string;
+}
+
 export interface CommandDictionary {
   id: number;
   name: string;
@@ -5,8 +10,10 @@ export interface CommandDictionary {
   description: string | null;
   allowed_sources: string[];
   allowed_destinations: string[];
+  required_data_fields: string[];
   expected_data_len: number;
   requires_ack: number;
   created_at: string | null;
   updated_at: string | null;
+  subsystems: CommandDictionarySubsystem[];
 }
